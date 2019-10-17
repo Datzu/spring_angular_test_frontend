63425614860fcfdf7b978c6b24969f6ffc912d2a
Adrian <Adrian@DESKTOP-GLB89L8.home>
2019-10-17 21:40:47 +0200

    Add new view working to get the user

---
+++ app/view3/view3.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('myApp.view3', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view3', {
+    templateUrl: 'view3/view3.html',
+    controller: 'View3Ctrl'
+  });
+}])
+
+.controller('View3Ctrl', [ '$scope', '$http',
+	function($scope, $http) {
+	
+		$scope.getUser = function() {
+			if ($scope.userId.trim().trim().length === 0) {
+				$scope.data = "";
+			} else {
+				getUser();
+			}
+			//getUser();
+		}
+		
+		function getUser() {
+			$http.get('http://localhost:9090/user/' + $scope.userId).
+			    then(function(response) {
+			    	var responseData = response.data;
+			    	if (responseData) {
+			    		$scope.data = responseData;
+			    	} else {
+			    		$scope.data = "";
+			    	}
+			    });
+		}
+
+	}
+]);
